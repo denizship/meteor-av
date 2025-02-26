2ec49814ce53446b49b07ff462db830d69cab350
denizship <deniz.alatas1@stu.bahcesehir.k12.tr>
2025-02-26 22:28:46 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,88 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Fire = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        `, Spaceship, 0, -100)
+    Fire.startEffect(effects.warmRadial, 100)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(Sprite2)
+    sprites.destroy(mySprite, effects.disintegrate, 500)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    let Othersprite: Sprite = null
+    scene.cameraShake(4, 500)
+    sprites.destroy(Othersprite, effects.disintegrate, 500)
+    Sprite2.startEffect(effects.fire, 200)
+    info.changeLifeBy(-1)
+})
+let projectile: Sprite = null
+let Meteor = 0
+let mySprite: Sprite = null
+let Sprite2: Sprite = null
+let Fire: Sprite = null
+let Spaceship: Sprite = null
+game.splash("Meteor avı")
+game.showLongText("Yön tuşları ile hareket et, A tuşuna basıp meteorları yok et", DialogLayout.Center)
+effects.starField.startScreenEffect()
+Spaceship = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+Spaceship.setStayInScreen(true)
+Spaceship.bottom = 120
+controller.moveSprite(Spaceship, 100, 100)
+info.setLife(3)
+game.onUpdateInterval(500, function () {
+    Meteor = 0
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 0, 74)
+    Meteor.setKind(SpriteKind.Enemy)
+    Fire.x = randint(10, 150)
+    game.gameOver(true)
+})
